fab01b6b7d4d9ff4467b6f67fa4b84cfadac79cf
lqx <liuquanxin@kanzhun.com>
2020-02-12 14:06:50 +0800

    add z.sh

--- main.sh
+++ main.sh
@@ -6,25 +6,25 @@ log() {
     echo "`date "+%Y-%m-%d %H:%M:%S"` INFO -- $@"
 }
 
-source git.sh
-echo 'source ~/bin/bashrc' >> ~/.bashrc
+#source git.sh
+#echo 'source ~/bin/bashrc' >> ~/.bashrc
 log "git done"
 
 # 大小写提示
-echo "set completion-ignore-case on" >> .inputrc
+echo "set completion-ignore-case on" >> ~/.inputrc
 
 # vim配置
-cat vi.conf>> ~/.vimrc
+cat vi.conf > ~/.vimrc
 log "vi done"
 
 # java配置
-echo 'JAVA_HOME=/home/lqx/jdk1.8.0_91
-CLASSPATH=$JAVA_HOME/lib
-PATH=$PATH:$JAVA_HOME/bin:~/bin
-export PATH JAVA_HOME CLASSPATH' >> ~/.profile
+#echo 'JAVA_HOME=/home/lqx/jdk1.8.0_91
+#CLASSPATH=$JAVA_HOME/lib
+#PATH=$PATH:$JAVA_HOME/bin:~/bin
+#export PATH JAVA_HOME CLASSPATH' >> ~/.profile
 
 #快捷命令 z 配置
-git clone https://github.com/rupa/z ~/bin/z
-echo 'source ~/bin/z/z.sh' >> ~/.bashrc
+#git clone https://github.com/rupa/z ~/bin/z
+echo 'source ~/linux-conf/z.sh' >> ~/.bashrc
 
 source ~/.bashrc
